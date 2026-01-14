542072690b376bad7df99659ad45fd26ee46b219
박민준 <p4comma6@gmail.com>
2026-01-14 14:55:20 +0900

    26/1/14

--- sort().py
+++ sort().py
@@ -18,3 +18,15 @@ print(lst)
 
 # 출력
 [4, 2, 1]
+
+# sorted() : “정렬된 새 리스트”를 만들어서 반환
+
+# 원본은 안 바뀜
+
+# 리스트 말고도(튜플, 문자열 등) **반복 가능한 것(iterable)**이면 대부분 됨
+
+nums = [3, 1, 2]
+new_nums = sorted(nums)
+
+print(nums)      # [3, 1, 2]  (원본 그대로)
+print(new_nums)  # [1, 2, 3]  (새로 만들어짐)
